e08531a65554c555ef41e3322fde2ce22ed41e28
Rudraksh Naik <rudrakshnaik8990@gmail.com>
2024-03-19 03:47:54 +0530

    Added snowball stemmer

--- src/stemming/stemming.py
+++ src/stemming/stemming.py
@@ -1,29 +1,42 @@
-from nltk.stem import PorterStemmer
+from nltk.stem import PorterStemmer , SnowballStemmer
 from nltk.tokenize import word_tokenize
-from abc import ABCMeta , abstractmethod
 
-# Initialize the Porter Stemmer
+# Initialize the Stemmers
+
+
 
 
 class Stemming : 
 
-    @staticmethod
-    @abstractmethod
-    def run(text_data) :
+    def __new__(cls) :
+        return cls
+
+    @classmethod
+    def run(cls,stemmer ,text_data) :
 
         # Initialize the Porter Stemmer
-        porter = PorterStemmer()
+        stemmer = cls.select_stemmer(stemmer)
 
         # Tokenize the text into words
         words = word_tokenize(text_data)
 
         # Perform stemming on each word
-        stemmed_words = [porter.stem(word) for word in words]
+        stemmed_words = [stemmer.stem(word) for word in words]
 
         # Join the stemmed words back into a single string
         stemmed_text = ' '.join(stemmed_words)
 
         return stemmed_text
+    
+
+    @classmethod
+    def select_stemmer(cls,stemmer_name) :
+
+            if stemmer_name == "porter" : 
+                return PorterStemmer()
+            if stemmer_name == "snowball" : 
+                return SnowballStemmer("english")
+
 
 
 
@@ -31,7 +44,7 @@ if __name__ == "__main__" :
         
     text_data = "Stemming is a process of reducing words to their root forms"
 
-    stemmed_text = Stemming.run(text_data)
+    stemmed_text = Stemming.run('snowball',text_data)
     print("Original Text:")
     print(text_data)
     print("\nStemmed Text:")
